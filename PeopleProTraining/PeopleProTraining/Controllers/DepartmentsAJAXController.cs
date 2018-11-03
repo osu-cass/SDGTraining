6e2888fc55d82bbe363aff8b60ddcad4635bcd6d
Ortega <drew.ortega@oregonstate.edu>
2018-11-02 17:42:04 -0700

    began work on ajax code

---
+++ PeopleProTraining/PeopleProTraining/Controllers/DepartmentsAJAXController.cs
@@ -0,0 +1,28 @@
+﻿using PeopleProTraining.Dal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PeopleProTraining.Controllers
+{
+    public class DepartmentsAJAXController : Controller
+    {
+        private PeopleProDataEntities db = new PeopleProDataEntities();
+
+        // GET: DepartmentsAJAX
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult List()
+        {
+            return Json(db.Departments, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult Add(Department department)
+        {
+            return Json(db.Departments.Add(department), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
