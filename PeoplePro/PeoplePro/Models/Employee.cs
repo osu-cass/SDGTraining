6dd280569927f1e914fed47cb78bcf9e317d9a8b
zeyad <zeyad@me.com>
2019-04-02 17:21:12 -0700

    Began working on deliverables

---
+++ PeoplePro/PeoplePro/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PeoplePro.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+
+        [Required, StringLength(60, MinimumLength = 1), Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$"), Required, StringLength(30)]
+        public string Department { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$"), StringLength(10), Required]
+        public string Building { get; set; }
+
+    }
+}
