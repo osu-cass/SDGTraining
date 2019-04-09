98bd95536561c73843478fdd4c03032b9fa9a6be
Phi Luu <philectron@users.noreply.github.com>
2019-04-09 13:39:20 -0700

    Add and pass unit test for HomeController

---
+++ PeopleProTraining/PeoplePro.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PeoplePro.Dal;
+using PeoplePro.Dal.Interfaces;
+using PeoplePro;
+using PeoplePro.Controllers;
+
+namespace PeoplePro.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // arrange
+            var controller = new HomeController();
+
+            // act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void About()
+        {
+            // arrange
+            HomeController controller = new HomeController();
+
+            // act
+            ViewResult result = controller.About() as ViewResult;
+
+            // assert
+            Assert.AreEqual("Your application description page.", result.ViewData["Message"]);
+        }
+
+        [TestMethod]
+        public void Contact()
+        {
+            // arrange
+            HomeController controller = new HomeController();
+
+            // act
+            ViewResult result = controller.Contact() as ViewResult;
+
+            // assert
+            Assert.AreEqual("Your contact page.", result.ViewData["Message"]);
+        }
+
+        [TestMethod]
+        public void Privacy()
+        {
+            // arrange
+            HomeController controller = new HomeController();
+
+            // act
+            ViewResult result = controller.Privacy() as ViewResult;
+
+            // assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
